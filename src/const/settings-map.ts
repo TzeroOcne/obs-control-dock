c09c835f40fc80f7afb792d937c2343d3fab6d0f
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-11-24 16:28:47 +0700

    feat(settings): add more preset
    add this pressets
    - star rail
    - duet night abyss
    - stella sora

--- src/const/settings-map.ts
+++ src/const/settings-map.ts
@@ -2,14 +2,23 @@ import type { OBSRequestTypes } from 'obs-websocket-js';
 
 const PresetSceneEnum = {
   GENSHIN: 'Genshin',
+  STAR_RAIL: 'Star Rail',
   ZENLESS_ZONE_ZERO: 'Zenless Zone Zero',
   HEAVEN_BURNS_RED: 'Heaven Burns Red',
   BLUE_ARCHIVE: 'Blue Archive',
+  DUET_NIGHT_ABYSS: 'Duet Night Abyss',
+  STELLA_SORA: 'Stella Sora',
 } as const;
 type PresetScene = (typeof PresetSceneEnum)[keyof typeof PresetSceneEnum];
 
 export const SceneSettings:Record<PresetScene, OBSRequestTypes['SetVideoSettings']> = {
   [PresetSceneEnum.GENSHIN]: {
+    baseWidth: 1920,
+    baseHeight: 1200,
+    outputWidth: 1920,
+    outputHeight: 1200,
+  },
+  [PresetSceneEnum.STAR_RAIL]: {
     baseWidth: 1920,
     baseHeight: 1200,
     outputWidth: 1920,
@@ -33,11 +42,26 @@ export const SceneSettings:Record<PresetScene, OBSRequestTypes['SetVideoSettings
     outputWidth: 2080,
     outputHeight: 1170,
   },
+  [PresetSceneEnum.DUET_NIGHT_ABYSS]: {
+    baseWidth: 2080,
+    baseHeight: 1170,
+    outputWidth: 2080,
+    outputHeight: 1170,
+  },
+  [PresetSceneEnum.STELLA_SORA]: {
+    baseWidth: 1920,
+    baseHeight: 1200,
+    outputWidth: 1920,
+    outputHeight: 1200,
+  },
 };
 
 export const SceneRecordDirectory:Record<PresetScene, string> = {
   [PresetSceneEnum.GENSHIN]: 'Genshin Impact',
+  [PresetSceneEnum.STAR_RAIL]: 'Star Rail',
   [PresetSceneEnum.ZENLESS_ZONE_ZERO]: 'Zenless Zone Zero',
   [PresetSceneEnum.HEAVEN_BURNS_RED]: 'Heaven Burns Red',
   [PresetSceneEnum.BLUE_ARCHIVE]: 'Blue Archive',
+  [PresetSceneEnum.DUET_NIGHT_ABYSS]: 'Duet Night Abyss',
+  [PresetSceneEnum.STELLA_SORA]: 'Stella Sora',
 };
